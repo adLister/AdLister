c335ce105f63602e8c0000a7d7167f0a22efefb4
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-10 15:58:36 -0500

    updated bootstrap and index

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,7 @@
 <?php
-
 require_once 'utils/Auth.php';
 require_once 'utils/Input.php';
 require_once 'utils/logger.php';
-require_once 'utils/Log.php';
-
-require_once 'database/db_connect.php';
-
 ?>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <!-- Latest compiled and minified CSS -->
@@ -17,5 +12,3 @@ require_once 'database/db_connect.php';
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="/public/css/custom.css">
