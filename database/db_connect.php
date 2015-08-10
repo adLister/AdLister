67a9af32ec3c31be229373e4f46b375c6e089ef6
Remington <remingtonwilliams@gmail.com>
2015-08-10 15:21:11 -0500

    index

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,5 +1,11 @@
 <?php
+define("DB_HOST", '127.0.0.1');
 
+define("DB_NAME", 'adlister_db');
+
+define("DB_USER", 'user');
+
+define("DB_PASS", 'password');
 // Get new instance of PDO object
 $dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 
