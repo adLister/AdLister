0b57360f336cf633592d91ece5e895f7793d9e9a
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-11 15:19:55 -0500

    background for welcome page

--- public/index.php
+++ public/index.php
@@ -32,9 +32,9 @@ if($_GET['page'] > $maxpage || !is_numeric($_GET['page']) || $_GET['page'] < 1){
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
     <link rel="stylesheet" href="/css/sidebar.css">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
     <link rel="stylesheet" href="/css/custom.css"> 
+
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 
 <body>
