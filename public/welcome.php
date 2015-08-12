ade3f7308efaf906830dd4966343473e6964e67a
Remington <remingtonwilliams@gmail.com>
2015-08-12 09:06:40 -0500

    working on user login and create

--- public/welcome.php
+++ public/welcome.php
@@ -2,23 +2,27 @@
 require_once '../bootstrap.php';
 
 
-session_start();
-$sessionId = session_id();
-
-$LOGGED_IN_USER = false;
-
-if (Input::has('username') && Input::has('password')){
-    $username = escape(trim(Input::get('username')));
-    $password = trim(Input::get('password'));
-        
-    if (isset($_POST['username'])){
-        Auth::attempt($username, $password);
+$errors = array();
+if(!empty($_POST)){
+    try { 
+        Input::getString('email');
+    }catch(Exception $e){
+        $errors[] = $e->getMessage(). ' for the email feild.';
     }
-}
 
-if(Auth::check()){
-    header("Location: index.php");
-    exit();
+    try { 
+        Input::getString('password');
+    }catch(Exception $e){
+            $errors[] = $e->getMessage() . ' for the password feild.';
+    }
+
+    if(empty($errors)){
+        $newPost = $dbc->prepare("INSERT INTO users(email, password) 
+        VALUES(:email,:password)");
+        $newPost->bindValue(':email', Input::getString('email'), PDO::PARAM_STR);
+        $newPost->bindValue(':password', Input::getString('password'), PDO::PARAM_STR);
+        $newPost->execute();
+    }
 }
 
 ?>
@@ -36,9 +40,9 @@ if(Auth::check()){
 
         <div id="login-form">
             <form>
-                <div id="username">
+                <div id="email">
                     </label>Email: </label><br>
-                    <input class="login" id="username" type="text" name="username" placeholder="Username" autofocus>
+                    <input class="login" id="input-email" type="text" name="email" placeholder="Email" autofocus>
                 </div>
                 <div id="password">
                     <label>Password: </label><br>
@@ -52,10 +56,8 @@ if(Auth::check()){
         <div id="sub-header">
             <h1>Sign Up!</h1>
         </div>
-        <form>
-            <!-- </label>Email: </label><br> -->
+        <form action="welcome.php" method="POST">
             <input class="new-user" id="new-user-email" type="text" name="email" placeholder="Email"><br>
-            <!-- <label>Password: </label><br> -->
             <input class="new-user" id="new-user-password" type="password" name="password" placeholder="Password"><br>
             <button id="submit-button" type="submit">Submit</button>
         </form>
