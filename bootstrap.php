919231db104d76f5ce6d0c0f572e3f3deaa83c81
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-12 15:59:40 -0500

    edited Ad class

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once 'utils/Auth.php';
 require_once 'utils/Input.php';
-// require_once 'models/Model.php';
+require_once 'models/Ad.php';
 require_once 'database/db_connect.php';
 
 $dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
