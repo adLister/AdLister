3701cc18fd6d1ad15237d9d19b0505771beae46e
Remington <remingtonwilliams@gmail.com>
2015-08-13 10:34:29 -0500

    fixed seeder

--- database/ads_seeder.php
+++ database/ads_seeder.php
@@ -8,27 +8,60 @@ $truncate = 'TRUNCATE ads';
 
 $dbc->exec($truncate);
 
-$ads = [
-    ['title' => 'Acadia', 'description' => 'ME', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Arches', 'description' => 'UT', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Badlands', 'description' => 'SD', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Big Bend', 'description' => 'TX', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Carlsbad Caverns', 'description' => 'NM', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Crater Lake', 'description' => 'OR', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Denali', 'description' => 'AL', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Everglades', 'description' => 'FL', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Glacier', 'description' => 'MT', 'image_url' => '/img/pingpong.jpeg', 'price' => '0'],
-    ['title' => 'Olympic', 'description' => 'WA', 'image_url' =>'/img/pingpong.jpeg', 'price' => '0']
+$categories = array(
+    'Accounting and Finance',
+    'Admin and Office',
+    'Appliances',
+    'Art/Media/Design',
+    'Arts and Crafts',
+    'Auto Parts',
+    'Automotive',
+    'Baby and kid',
+    'Biotech and Science',
+    'Books',
+    'Business/Mgmt',
+    'Cars and Trucks',
+    'Computer and Technology',
+    'Computers and Electronics',
+    'Customer Service',
+    'Education',
+    'Event',
+    'Furniture',
+    'Human Resources',
+    'Internet Engineers',
+    'Legal',
+    'Legal/Peralegal',
+    'Lessons',
+    'Medical/Health',
+    'Music',
+    'Pet',
+    'Real Estate',
+    'Realator',
+    'Salon/Spa/Fitness',
+    'Security',
+    'Software/QA/DBA',
+    'Sports and Outdoors',
+    'Technical Support',
+    'Tools',
+    'Transport',
+    'Video Gaming',
+    'Writing/Editing'
+    );
+  
+$ad = [
+    'title' => 'toothbrush', 'description' => 'like new', 'image_url' => 'pingpong.jpeg','category' => "$category", 'price' => '5'
 ];
 
-    $stmt = $dbc->prepare("INSERT INTO ads(title, description, price image_url) VALUES(:title, :description, :price, :image_url)");
-foreach ($ads as $ad) {
-     $stmt ->bindValue(':title', $ad['title'], PDO::PARAM_STR);
-     $stmt ->bindValue(':description', $ad['description'], PDO::PARAM_STR);
-     $stmt ->bindValue(':price', $ad['price'], PDO::PARAM_STR);
-     $stmt ->bindValue(':image_url', $ad['image_url'], PDO::PARAM_STR);
+foreach ($categories as $category) {
 
+    $stmt = $dbc->prepare("INSERT INTO ads(title, description, price, category, image_url)
+     VALUES(:title, :description, :price, :category, :image_url)");
+    $stmt ->bindValue(':title', $ad['title'], PDO::PARAM_STR);
+    $stmt ->bindValue(':description', $ad['description'], PDO::PARAM_STR);
+    $stmt ->bindValue(':price', $ad['price'], PDO::PARAM_STR);
+    $stmt ->bindValue(':category', $category, PDO::PARAM_STR);
+    $stmt ->bindValue(':image_url', $ad['image_url'], PDO::PARAM_STR);
     $stmt->execute();
-}
     echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
     
+}
