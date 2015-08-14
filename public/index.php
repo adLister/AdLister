d9c93df70730a200cce7407c1c4c407d2ee4f78b
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-14 13:32:49 -0500

    started front contoller

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
 <?php
+switch ($_SERVER['REQUEST_URI']) {
+    case '/home':
+        include 'home.php';
+        break;
+    case '/ads/show':
+        include 'ads.show.php';
+        break;
+    case '/category=$category':
+        include 'categories.php';
+        break;
+    case '/ads/create':
+        include 'ads.create.php';
+        break;
+    case '/ads/edit':
+        include 'ads.edit.php';
+        break;
+    case '/myaccount':
+        include 'myaccount.php';
+        break;
+    default:
+        include 'welcome.php';
+        break;
+}
